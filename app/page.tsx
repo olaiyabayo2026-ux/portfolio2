f4e5047ef37ae411e7d709d09376b2b4db4a6f06
Olaiya Adebayo <olaiyabayo2026@gmail.com>
2026-06-09 17:58:00 -0700

    Added CV download button

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,14 @@ import Contact from "@/components/Contact";
 export default function Home() {
   return (
     <>
-      <Hero />
+      <Hero /><a 
+  href="https://github.com/olaiyabayo2026-ux/portfolio2/blob/main/cv-olaiya-adebayo.pdff"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700"
+>
+  Download My CV
+</a>
       <About />
       <Skills />
       <Experience />
